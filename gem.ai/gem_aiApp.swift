bfb84e9bbc4985b96a93e1b57d802efedab335d5
Oleh Martyn <olegmartin12@gmail.com>
2025-10-04 14:31:13 +0200

    Initial Commit

---
+++ gem.ai/gem_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  gem_aiApp.swift
+//  gem.ai
+//
+//  Created by Oleh Martyn on 04/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct gem_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
